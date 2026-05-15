6213c9c7584e7c323b1b3625ca591a5e6436146c
dnieblesdev <dnieblesp@gmail.com>
2026-05-15 00:41:09 -0500

    debug(bot): use dynamic import to surface startup errors

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,1 +1,5 @@
-import "./app/core/index.ts";
+// Dynamic import so we can catch startup errors
+import("./app/core/index.ts").catch((err) => {
+  console.error("BOT STARTUP ERROR:", err);
+  process.exit(1);
+});
